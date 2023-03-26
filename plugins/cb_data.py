6305cfb0a87d913b6bd73cd9de578181cbdfb057
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-03-26 23:06:36 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -22,7 +22,7 @@ API_ID = int(os.environ.get("API_ID", "26305247"))
 
 API_HASH = os.environ.get("API_HASH", "20ca7e6687c281e11782856c7efd0ff7")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQAelg_pvzw_veEjEpzL0AdWM4H4_VgbSg5qckh1vpmqjnnqGOZ-zSL_mqSpfPj1yEURAGZwphmNPQrpixzVZ0kqHp7ncKz4ixfzLE199ov10q14bLlHI8pgG9VWMH9gRi8Cdoq-xh2euP7BA36kIxTQeGHUQABo_WttQMQJF6dfCF_d5k9qv5hXoWzbZfPwmf-EUUAhYd7dwTPG2vCSVjp_daWzdGGOgG7AJksEMQpoSQRhbJgG6Qpxz1nu67X4VCndBFGkjGHsUkRCLDb-iUkbIUvvZQCRYCpkZjS8q67KPT0etl2u2BpbfRXLFRkBBjGkiksVUoam18IoLLR16zaEAAAAATMiN8EA")
 
 ADMIN = os.environ.get("ADMIN", "5152847809")
 
