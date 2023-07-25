e1fdaa3bb3d9d8dfdae47c317c11c55ac5690929
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-07-25 14:36:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,7 @@ API_ID = int(os.environ.get("API_ID", "24237617"))
 
 API_HASH = os.environ.get("API_HASH", "8ec32b4085f1e3cf56c34a6afe1cc590")
 
-STRING = os.environ.get("STRING", "BQFx1jEAqxjYofT4TsIc1kjkgYzfnsvcUUzLyPTVPEtEGZMTl1lhzIpXBWnTIDkuhSuWJGlhEGGpgFI89_NhlUWt9Hbd8eCLBJ0CW48YtUx6UrP6HeVNOHYEsOC2PCRJB_tlwi6Gwjyp0VW0pfp4qavefwuzOgFgJF20ve_lLgsfbNs5nz5jOXPmGIdKvwnNwGOAlYhchrhoixYjnSIo5kKLYG98okb5Sl4yFADKKd6J1Lx-bDHeX3eQ9_ZArcuLBZdRWTpZ56wnFbq2zs_cXig-qRjHxHU564yfX31XiB-4wOgg-nehylp67QI_9bcDq2AEgMPUa_rCul6y2otKr038QqEzBwAAAAF7vgGsAA")
-
+STRING = os.environ.get("STRING", "")
 
 bot = Client(
 
